edee2efa93b0559de65302563035a047cdd3aace
Brandon Grams <bgrams429@gmail.com>
2021-03-20 13:27:18 -0500

    Clean up session get

--- fredio/session.py
+++ fredio/session.py
@@ -4,7 +4,7 @@ import asyncio
 import itertools
 import logging
 from functools import partial
-from typing import Any, Awaitable, List, Dict, Generator, Optional
+from typing import Any, List, Dict, Optional
 
 import jsonpath_rw
 from aiohttp import ClientSession, ClientResponseError
@@ -15,20 +15,6 @@ from . import locks
 
 
 logger = logging.getLogger(__name__)
-
-
-def iter_offsets(count: int, limit: int, offset: int) -> Generator[int, None, None]:
-    """
-    Generator yielding new offsets. The offset is incremented by limit until
-    it surpasses count.
-
-    :param count: count
-    :param limit: limit
-    :param offset: offset
-    """
-    while offset + limit < count:
-        offset += limit
-        yield offset
 
 
 class Session(object):
@@ -100,9 +86,9 @@ class Session(object):
                   retries: int = 3,
                   **parameters) -> List[Dict]:
         """
-        Will await a single request to get the first batch of data before executing subsequent
-        requests (if required) according to offset logic. Jsonpath query is optionally executed
-        on json response data from each request.
+        Will await a single request to get the first batch of data before executing
+        subsequent requests (if required) according to offset logic. Jsonpath query
+        is optionally executed on json response data from each request.
 
         :param url: yarl URL
         :param jsonpath: Optional jsonpath query to process response data
@@ -113,30 +99,32 @@ class Session(object):
         if parameters:
             url = url.update_query(**parameters)
 
-        init_response = await self.request("GET", url, retries=retries)
+        getter = partial(self.request, "GET", retries=retries)
 
-        ir_count = init_response.get("count")
-        ir_limit = init_response.get("limit")
-        ir_offset = init_response.get("offset")
+        response = [await getter(url)]
 
-        logger.debug("Count: %s, Limit: %s, Offset: %s" % (ir_count, ir_limit, ir_offset))
+        count = response[0].get("count")
+        limit = response[0].get("limit")
+        offset = response[0].get("offset")
 
-        results = [init_response]
-        if any((ir_count, ir_limit, ir_offset)):
+        logger.debug("Count: %s, Limit: %s, Offset: %s" % (count, limit, offset))
 
-            coros = [
-                self.request("GET", url.update_query(offset=offset), retries=retries)
-                for offset in iter_offsets(ir_count, ir_limit, ir_offset)
-            ]
+        if any((count, limit, offset)):
+
+            coros = list(map(
+                lambda x: getter(url=url.update_query(offset=x)),
+                range(limit + offset, count, limit)
+            ))
 
             logger.debug("Planning %s additional requests" % len(coros))
-            results.extend(await asyncio.gather(*coros))
+            response.extend(await asyncio.gather(*coros))
 
         if jsonpath:
             parsed = jsonpath_rw.parse(jsonpath)
-            mapped = map(lambda x: [i.value for i in parsed.find(x)], results)
+            mapped = map(lambda x: [i.value for i in parsed.find(x)], response)
             return list(itertools.chain.from_iterable(mapped))
-        return results
+
+        return response
 
     async def close(self) -> None:
         """
